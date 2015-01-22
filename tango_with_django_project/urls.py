42da52e19de14a05f50640f311ee5f96a97121bc
yang <classmateyang@hotmail.com>
2015-01-22 10:58:51 -0500

    add a map to index

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^rango/', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+
+
+
 )
